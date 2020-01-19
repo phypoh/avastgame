360e396f8b2e9824a3d873ef5416549c12f4016c
ag3nt3154 <ag3nt3154@gmail.com>
2020-01-19 02:31:03 +0000

    update expansion pack

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Main game script
 var config = {
     type: Phaser.AUTO,
-    width: 1000,
+    width: 1200,
     height: 600,
     physics: {
         default: 'arcade',
@@ -49,28 +49,34 @@ function preload ()
     this.load.image('background', 'assets/VFX/background.png');
     this.load.image('ground', 'assets/VFX/platform.png');
     this.load.image('end_screen', 'assets/VFX/end_screen.png');
+    this.load.image('computer', 'assets/VFX/computer.png');
+
     this.load.spritesheet('player', 'assets/VFX/player/player.png', { frameWidth: 100, frameHeight: 100 });
     this.load.spritesheet('trojan', 'assets/VFX/trojan/trojan.png', { frameWidth: 100, frameHeight: 100 });
+    this.load.spritesheet('phish', 'assets/VFX/phish/phish.png', { frameWidth: 100, frameHeight: 100 });
+    this.load.spritesheet('adware', 'assets/VFX/adware/adware.png', { frameWidth: 100, frameHeight: 100 });
     this.load.spritesheet('botnet', 'assets/VFX/botnet/botnet.png', { frameWidth: 100, frameHeight: 100 });   // Trojan horse spritesheet
     this.load.spritesheet('ransom', 'assets/VFX/ransom/ransom.png', { frameWidth: 100, frameHeight: 100 });   // Ransomware spritesheet
+    
     this.load.spritesheet('splat', 'assets/VFX/splat/splat.png', { frameWidth: 100, frameHeight: 100 });     // Virus death spritesheet
     this.load.spritesheet('playerhit', 'assets/VFX/playerhit/playerhit.png', { frameWidth: 300, frameHeight: 200 });
+    this.load.spritesheet('playerhitjet', 'assets/VFX/playerhitjet/playerhitjet.png', { frameWidth: 300, frameHeight: 200 });
 }
 
 function create ()
 {
     // Create background
     bg = this.physics.add.staticGroup();
-    bg.create(500, 300, 'background');
+    bg.create(600, 300, 'background').setScale(1.3).refreshBody();
     
     // Create ending screen
-    end_screen = this.add.image(500, 300, 'end_screen');
+    end_screen = this.add.image(600, 300, 'end_screen');
     end_screen.visible = false;
 
     <!-- Create Platform -->
     // Note to self: to clean up
     platforms = this.physics.add.staticGroup();
-    platforms.create(500, 568, 'ground').setScale(2.5).refreshBody();
+    platforms.create(600, 600, 'ground').setScale(3).refreshBody();
 
     <!-- Create Player -->
     player = this.physics.add.sprite(220, 450, 'player');
@@ -80,10 +86,11 @@ function create ()
     <!-- Create Computer -->
     //comp = this.physics.add.sprite(30, 525, 'end_screen');
     comp = this.physics.add.staticGroup();
-    comp.create(10, 300, 'ground').setScale(0.1, 20).refreshBody();
+    comp.create(70, 300, 'computer');
 
     <!-- Create Enemy -->
     enemies = this.physics.add.group({allowGravity: false});
+    splats = this.physics.add.group({allowGravity: false});
     spawnEnemy();
 
     <!-- Collisions -->
@@ -150,8 +157,19 @@ function create ()
         repeat: -1
     });
 
+    this.anims.create({
+        key: 'adware_move',           
+        frames: this.anims.generateFrameNumbers('adware', { start: 0, end: 1 }),
+        frameRate: 5,
+        repeat: -1
+    });
 
-
+    this.anims.create({
+        key: 'phish_move',           
+        frames: this.anims.generateFrameNumbers('phish', { start: 0, end: 1 }),
+        frameRate: 5,
+        repeat: -1
+    });
 
     this.anims.create({
         key: 'enemy_dead',           
@@ -160,8 +178,14 @@ function create ()
     });
 
     this.anims.create({
-        key: 'hammer',           
+        key: 'hammer_ground',           
         frames: this.anims.generateFrameNumbers('playerhit', { start: 0, end: 6 }),
+        frameRate: 15,
+    });
+
+    this.anims.create({
+        key: 'hammer_jet',           
+        frames: this.anims.generateFrameNumbers('playerhitjet', { start: 0, end: 6 }),
         frameRate: 15,
     });
         //
@@ -171,7 +195,7 @@ function create ()
 function update ()
 {
     // Enemy movement animation
-    if (!gameOver)
+    if (gameOver == false && kill_enemy == false)
             enemies.children.iterate(child => 
             {
                 if (child.name == 'trojan')
@@ -186,6 +210,14 @@ function update ()
                 {
                     child.anims.play('botnet_move', true)
                 }
+                else if (child.name == 'adware')
+                {
+                    child.anims.play('adware_move', true)
+                }
+                else if (child.name == 'phish')
+                {
+                    child.anims.play('phish_move', true)
+                }
             });
 
 
@@ -195,20 +227,21 @@ function update ()
     // Increase gravity by increasing score
     player.body.gravity.y = Math.min(1200, (player_speed_incre + 600));
     // Jump up
-    if (cursors.up.isDown) 
-    {
-        // Increase velocity with score
-        player.setVelocityY(Math.max(-600, (-250 - player_speed_incre)));
-        player.anims.play('up', true); 
-    }
-    else if (player.body.touching.down)
-    {
-        player.anims.play('move', true);
-    }
-    else
-    {
-        player.anims.play('up_stall', true);
-    }
+    if (kill_enemy_player == false)
+        if (cursors.up.isDown) 
+        {
+            // Increase velocity with score
+            player.setVelocityY(Math.max(-600, (-250 - player_speed_incre)));
+            player.anims.play('up', true); 
+        }
+        else if (player.body.touching.down)
+        {
+            player.anims.play('move', true);
+        }
+        else
+        {
+            player.anims.play('up_stall', true);
+        }
 
     // Calculate cone within which the n+1 enemy should spawn
     cone_size = Math.abs(counter * Math.max(-600, (-250 - player_speed_incre))/70);
@@ -278,10 +311,15 @@ function spawnEnemy ()
     else if (enemy_type == 4)
     {
         var enemy = enemies.create(900, getRandomInt(Math.max(100, prev_enemy_height - cone_size), 
-            Math.min(525, prev_enemy_height + cone_size)) , 'trojan');
-        enemy.name = 'trojan';
+            Math.min(525, prev_enemy_height + cone_size)) , 'adware');
+        enemy.name = 'adware';
     }
-
+    else if (enemy_type == 5)
+    {
+        var enemy = enemies.create(900, getRandomInt(Math.max(100, prev_enemy_height - cone_size), 
+            Math.min(525, prev_enemy_height + cone_size)) , 'phish');
+        enemy.name = 'phish';
+    }
     prev_enemy_height = enemy.y;
 
     enemy.setBounce(0);
@@ -310,29 +348,37 @@ function killEnemy (player, enemy)
 
 
     kill_enemy = true;
-    kill_enemy_player = true;
-    enemy.setVelocity(0,0);
-    player.setVelocity(0,0);
-    position_x = enemy.x
-    position_y = enemy.y
+	kill_enemy_player = true;
+	enemy.setVelocity(0,0);
+	player.setVelocity(0,0);
+
+	if (player.y > 450 && kill_enemy_player)
+	    player.anims.play('hammer_ground', true);
+	else
+		player.anims.play('hammer_jet', true);
+	player.once('animationcomplete',() => {
+		console.log('animationcomplete')
 
 
+		kill_enemy_player = false;
+		enemy.disableBody(true, true);
 
-    player.anims.play('hammer', true);
-    player.once('animationcomplete',() => {
-        console.log('animationcomplete')
-        player.anims.play('hammer', false);
-        enemy.anims.play('enemy_dead', true);
-        kill_enemy_player = false;
-        enemy.setPosition(position_x+20, position_y+20);
-        enemy.once('animationcomplete',() => {
-            console.log('animationcomplete')
-            enemy.disableBody(true, true);
-            kill_enemy = false;
-            
 
-        })
-    })
+		var splat = splats.create(enemy.x, enemy.y, 'splat');
+		splat.setBounce(0);
+		splat.setCollideWorldBounds(true);
+		splat.setVelocity(0, 0);
+		splat.body.AllowGravity = false;
+
+		splat.anims.play('enemy_dead', true);
+		splat.once('animationcomplete',() => {
+			console.log('animationcomplete')
+			splat.disableBody(true, true);
+			kill_enemy = false;
+				
+			//enemy.destroy()
+		})
+	})
 }
 
 function wipeEnemy (bg, enemy)
